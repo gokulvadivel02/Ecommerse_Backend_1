40f97512e49990065034000ac4eca11605f111d5
gokulvadivel02 <gokulvadivel002@gmail.com>
2025-09-04 18:41:28 +0530

    Initial commit

---
+++ src/main/java/com/Ecommerse/Backend/DemoController.java
@@ -0,0 +1,12 @@
+package com.Ecommerse.Backend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello Backend" ;
+    }
+}
